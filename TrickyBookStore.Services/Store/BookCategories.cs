70c6e764f9bbe65ed8c4c7c3f0c04f1f63981556
Đặng Thanh Hiếu <hieu.dang.thanh@preciofishbone.se>
2023-03-16 13:42:30 +0700

    update data

--- TrickyBookStore.Services/Store/BookCategories.cs
+++ TrickyBookStore.Services/Store/BookCategories.cs
@@ -7,6 +7,10 @@ namespace TrickyBookStore.Services.Store
     {
         public static readonly IEnumerable<BookCategory> Data = new List<BookCategory>
         {
+            new BookCategory() { Id = 1, Title = "Programming"},
+            new BookCategory() { Id = 2, Title = "Cooking"},
+            new BookCategory() { Id = 3, Title = "History"},
+            new BookCategory() { Id = 4, Title = "Science"},
         };
     }
 }
--- TrickyBookStore.Services/Store/Subscriptions.cs
+++ TrickyBookStore.Services/Store/Subscriptions.cs
@@ -7,46 +7,58 @@ namespace TrickyBookStore.Services.Store
     {
         public static readonly IEnumerable<Subscription> Data = new List<Subscription>
         {
-            new Subscription { Id = 1, SubscriptionType = SubscriptionTypes.Paid, Priority = ...,
+            new Subscription { Id = 1, SubscriptionType = SubscriptionTypes.Paid, Priority = 2,
                 PriceDetails = new Dictionary<string, double>
                 {
                    
                 }
             },
-            new Subscription { Id = 2, SubscriptionType = SubscriptionTypes.Free, Priority = ...,
+            new Subscription { Id = 2, SubscriptionType = SubscriptionTypes.Free, Priority = 1,
                 PriceDetails = new Dictionary<string, double>
                 {
 
                 }
             },
-            new Subscription { Id = 3, SubscriptionType = SubscriptionTypes.Premium, Priority = ...,
+            new Subscription { Id = 3, SubscriptionType = SubscriptionTypes.Premium, Priority = 3,
                 PriceDetails = new Dictionary<string, double>
                 {
                    
                 }
             },
-            new Subscription { Id = 4, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = ...,
+            new Subscription { Id = 4, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = 4,
                 PriceDetails = new Dictionary<string, double>
                 {
-                  
+                    
                 }              
             },
-            new Subscription { Id = 5, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = ...,
+            new Subscription { Id = 5, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = 4,
                 PriceDetails = new Dictionary<string, double>
                 {
-                    { "FixPrice", 75 },
-                    ....
+                    { "Discount", 0.75 },
                 },
                 BookCategoryId = 1
             },
-            new Subscription { Id = 6, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = ...,
+            new Subscription { Id = 6, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = 4,
                 PriceDetails = new Dictionary<string, double>
                 {
-                    { "FixPrice", 75 },
-                    ....
+                    { "Discount", 0.75 }
                 },
                 BookCategoryId = 3
-            }
+            },
+            new Subscription { Id = 7, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = 4,
+                PriceDetails = new Dictionary<string, double>
+                {
+                    { "Discount", 0.75 }
+                },
+                BookCategoryId = 2
+            },
+            new Subscription { Id = 7, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = 4,
+                PriceDetails = new Dictionary<string, double>
+                {
+                    { "Discount", 0.75 }
+                },
+                BookCategoryId = 4
+            },
         };
     }
 }
